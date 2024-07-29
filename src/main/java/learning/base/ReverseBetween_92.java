40a03e7a337e6af8dad619640e1dc48fded4c9e2
nana <1179102690@qq.com>
2024-07-29 17:30:19 +0800

    feat(base):92 reverseListNode

---
+++ src/main/java/learning/base/ReverseBetween_92.java
@@ -0,0 +1,39 @@
+package learning.base;
+
+import module.ListNode;
+
+/**
+ * 92. 反转链表 II
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ */
+public class ReverseBetween_92 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode res = new ReverseBetween_92().reverseBetween(head, 2, 4);
+        for(;res!=null;res=res.next) {
+            System.out.println(res.val);
+        }
+    }
+}
